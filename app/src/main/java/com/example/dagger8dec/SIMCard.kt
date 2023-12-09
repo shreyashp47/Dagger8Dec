12ee62e02d87a38d91af303c13c1cd1c68edf888
Shreyash <shreyashp47@gmail.com>
2023-12-09 11:04:24 +0530

    field injection

--- app/src/main/java/com/example/dagger8dec/SIMCard.kt
+++ app/src/main/java/com/example/dagger8dec/SIMCard.kt
@@ -3,6 +3,9 @@ package com.example.dagger8dec
 import android.util.Log
 
 class SIMCard(private  val serviceProvider: ServiceProvider) {
+
+//Field injection
+//    public  lateinit var serviceProvider: ServiceProvider
     init {
         Log.i("MYTAG","SIM Card Constructed")
     }
